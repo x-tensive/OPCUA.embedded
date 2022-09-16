43b82651ed16e100fe4f3d1d39edeeb9cd7d18ff
Alexey Belov <Alexey.Belov@x-tensive.com>
2022-09-17 01:01:31 +0300

    - try scripts

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,3 +1,29 @@
+const https = require("https");
 const updateLinks = require("./update-wiki-links");
 
-updateLinks(null);
+const token = "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";
+
+var options = {
+    host: "api.github.com",
+    path: "/repos/x-tensive/OPCUA.embedded/releases/74103366/assets",
+    method: "GET",
+    headers: {
+        "User-Agent": "update-wiki-links",
+        authorization: "token " + token,
+        "Content-Type": "application/json"
+    }
+};
+
+https.request(options, function(response) {
+    var responseBody = "";
+    response.setEncoding("utf8");
+    response.on("data", function (chunk) {
+        responseBody += chunk;
+    });
+    response.on("end", function () {
+        var data = JSON.parse(responseBody);
+        console.log(data);
+    });
+}).end();
+
+//updateLinks(null);
