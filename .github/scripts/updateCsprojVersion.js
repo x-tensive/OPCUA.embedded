fa864240ec94b441ec51660168ba8904ad3a05a8
Alexey Belov <Alexey.Belov@x-tensive.com>
2022-11-11 15:44:18 +0200

    - github actions

--- .github/scripts/updateCsprojVersion.js
+++ .github/scripts/updateCsprojVersion.js
@@ -8,9 +8,9 @@ function update(csprojFileName, version)
     const cVer = version.substring(1);
 
     fs.readFile(csprojFileName, { encoding: "utf8" }, (err, data) => {
-        data = data.replace(/<Version>\d\.\d\.\d<\/Version>/, "<Version>" + cVer + "</Version>");
-        data = data.replace(/<AssemblyVersion>\d\.\d\.\d\.\d<\/AssemblyVersion>/, "<AssemblyVersion>" + cVer + ".0</AssemblyVersion>");
-        data = data.replace(/<FileVersion>\d\.\d\.\d\.\d<\/FileVersion>/, "<FileVersion>" + cVer + ".0</FileVersion>");
+        data = data.replace(/<Version>\d+\.\d+\.\d+<\/Version>/, "<Version>" + cVer + "</Version>");
+        data = data.replace(/<AssemblyVersion>\d+\.\d+\.\d+\.\d+<\/AssemblyVersion>/, "<AssemblyVersion>" + cVer + ".0</AssemblyVersion>");
+        data = data.replace(/<FileVersion>\d+\.\d+\.\d+\.\d+<\/FileVersion>/, "<FileVersion>" + cVer + ".0</FileVersion>");
 
         console.log(data);
 
